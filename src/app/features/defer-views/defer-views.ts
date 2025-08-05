dac0ce9df492878e06f3cf741291610956aa10e2
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-05 16:51:56 -0400

    feat: implement defer-views component

---
+++ src/app/features/defer-views/defer-views.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-views',
+  imports: [],
+  templateUrl: './defer-views.html',
+  styleUrl: './defer-views.scss'
+})
+export class DeferViews {
+
+}
