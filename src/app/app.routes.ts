8ec6e31b0248fe9d56eaf34c0da9208f578f3caf
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-05 17:56:02 -0400

    feat(routing): implement dashboard routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard').then((c) => c.Dashboard),
+    children: [
+      {
+        path: 'change-detection',
+        loadComponent: () =>
+          import('./features/change-detection/change-detection').then(
+            (c) => c.ChangeDetection
+          ),
+      },
+      {
+        path: 'control-flow',
+        loadComponent: () =>
+          import('./features/control-flow/control-flow').then(
+            (c) => c.ControlFlow
+          ),
+      },
+      {
+        path: 'defer-options',
+        loadComponent: () =>
+          import('./features/defer-options/defer-options').then(
+            (c) => c.DeferOptions
+          ),
+      },
+      {
+        path: 'defer-views',
+        loadComponent: () =>
+          import('./features/defer-views/defer-views').then(
+            (c) => c.DeferViews
+          ),
+      },
+      {
+        path: 'user/:id',
+        loadComponent: () => import('./features/user/user').then((c) => c.User),
+      },
+      {
+        path: 'user-list',
+        loadComponent: () =>
+          import('./features/users/users').then((c) => c.Users),
+      },
+      {
+        path: 'view-transition',
+        loadComponent: () =>
+          import('./features/view-transition/view-transition').then(
+            (c) => c.ViewTransition
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+];
