a49b0169fd2b072fe574698385a30f62b967ac80
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-05 17:15:02 -0400

    feat(ui): implement users-loader on shared components

---
+++ src/app/shared/users-loader/users-loader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  imports: [],
+  templateUrl: './users-loader.html',
+  styleUrl: './users-loader.scss'
+})
+export class UsersLoader {
+
+}
