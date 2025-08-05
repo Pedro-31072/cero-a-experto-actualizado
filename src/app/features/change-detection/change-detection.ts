fb4af46924d60e65bd995f8b49d0c599815f4ce1
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-05 16:45:12 -0400

    feat: implement change-detection component

---
+++ src/app/features/change-detection/change-detection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-detection',
+  imports: [],
+  templateUrl: './change-detection.html',
+  styleUrl: './change-detection.scss'
+})
+export class ChangeDetection {
+
+}
