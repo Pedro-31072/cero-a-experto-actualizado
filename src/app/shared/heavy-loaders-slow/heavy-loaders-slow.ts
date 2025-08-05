5c90a00d43132cbbcbf6e3e2df9aa42903608259
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-05 17:10:50 -0400

    feat(ui): implement heavy-loaders-slow on shared components

---
+++ src/app/shared/heavy-loaders-slow/heavy-loaders-slow.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  imports: [],
+  templateUrl: './heavy-loaders-slow.html',
+  styleUrl: './heavy-loaders-slow.scss'
+})
+export class HeavyLoadersSlow {
+
+}
