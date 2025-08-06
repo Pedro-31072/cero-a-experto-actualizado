09614f81849a131fa1a2dc6a9da47cc2ac75fba3
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-06 16:16:48 -0400

    feat(ui) implement @if block on control-flow component

--- src/app/features/control-flow/control-flow.ts
+++ src/app/features/control-flow/control-flow.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
   imports: [],
   templateUrl: './control-flow.html',
-  styleUrl: './control-flow.scss'
+  styleUrl: './control-flow.scss',
 })
 export class ControlFlow {
+  public showContent = signal(false);
+  public toogleContent() {
+    this.showContent.update((value) => !value);
 
+  }
 }
