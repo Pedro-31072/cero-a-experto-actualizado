467474cc85245c7f89d86ec57b92d533921f65f7
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-06 17:30:07 -0400

    feat(ui): implement reusable title component for UI consistency

--- src/app/shared/title/title.ts
+++ src/app/shared/title/title.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   imports: [],
   templateUrl: './title.html',
-  styleUrl: './title.scss'
+  styleUrl: './title.scss',
 })
 export class Title {
-
+  protected _title = signal('');
+  protected _withBold = signal(false);
+  @Input({ required: true }) set setTitle(value: string) {
+    this._title.set(value);
+  }
+  @Input({ transform: booleanAttribute }) set withBold(value: boolean) {
+    this._withBold.set(value);
+  }
 }
