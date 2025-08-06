06358eb229c0a40a0ae968a350610acd1e195b6f
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-06 19:10:29 -0400

    feat(page): implement change-detection page

--- src/app/features/change-detection/change-detection.ts
+++ src/app/features/change-detection/change-detection.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, OnInit, signal } from '@angular/core';
+import { Title } from "@shared/title/title";
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  imports: [JsonPipe, Title],
+  changeDetection:ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.html',
-  styleUrl: './change-detection.scss'
+  styleUrl: './change-detection.scss',
 })
-export class ChangeDetection {
-
+export class ChangeDetection implements OnInit {
+  ngOnInit(): void {
+  }
+  public frameworkSelected = computed(()=>{
+    return this.frameworkAsSignal().name
+  })
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: '2016',
+  });
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: '2016',
+  };
+  constructor() {
+    setTimeout(()=>{
+      this.frameworkAsProperty.name="React"
+      this.frameworkAsSignal.update(value=>({
+        ...value,
+        name:"React"
+      }))
+    },3000)
+  }
 }
