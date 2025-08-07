481abb1b53bc43e7a5f2333d091bf61a5ee84bf0
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-06 20:20:42 -0400

    feat(shared): implement reusable heavy-load-component for test defer blocks

--- src/app/shared/heavy-loaders-slow/heavy-loaders-slow.ts
+++ src/app/shared/heavy-loaders-slow/heavy-loaders-slow.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass, } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
-  imports: [],
+  imports: [NgClass, ],
   templateUrl: './heavy-loaders-slow.html',
-  styleUrl: './heavy-loaders-slow.scss'
+  styleUrl: './heavy-loaders-slow.scss',
 })
 export class HeavyLoadersSlow {
-
+  @Input({ required: true }) cssClass!: string;
+  constructor() {
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+    console.log('Componente Cargado...');
+  }
 }
