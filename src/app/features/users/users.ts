b00d0305cf021d470ca5f6bf161dfafbe2f1e10d
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-08 15:20:07 -0400

    feat(users): add title in users component

--- src/app/features/users/users.ts
+++ src/app/features/users/users.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { UsersService } from '@services/users';
+import { Title } from '@shared/title/title';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [Title],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
