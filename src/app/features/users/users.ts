9f65d24d95b91054d65c4fb59363a4bec9a0d82e
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-08 11:28:40 -0400

    feat(users): inject UsersService into Users component

--- src/app/features/users/users.ts
+++ src/app/features/users/users.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from '@services/users';
 
 @Component({
   selector: 'app-users',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './users.scss'
 })
 export class Users {
+  constructor(private usersService:UsersService) {}
 
 }
