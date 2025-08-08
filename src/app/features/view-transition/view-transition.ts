bcd6eac1034ba1ab05c959e1c0946193d3415351
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-07 20:12:46 -0400

    feat(page): add content to view transition page

--- src/app/features/view-transition/view-transition.ts
+++ src/app/features/view-transition/view-transition.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Title } from "@shared/title/title";
 
 @Component({
   selector: 'app-view-transition',
-  imports: [],
+  imports: [Title],
   templateUrl: './view-transition.html',
   styleUrl: './view-transition.scss'
 })
