1fd2960ca2931f7ef8d19f1d5cd841a7fb1c455c
Pedro Julio Soto Tori <pedrojuliosototori1@gmail.com>
2025-08-10 10:05:03 -0400

    feat(users): implement user component logic and template

--- src/app/features/user/user.ts
+++ src/app/features/user/user.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, effect, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IUser } from '@interfaces/req.response';
+import { UsersService } from '@services/users';
+import { Title } from '@shared/title/title';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, switchMap, tap } from 'rxjs';
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [Title],
   templateUrl: './user.html',
-  styleUrl: './user.scss'
+  styleUrl: './user.scss',
 })
-export class User {
-
+export class User implements OnInit {
+  public usersService = inject(UsersService);
+  public route = inject(ActivatedRoute);
+  private params = this.route.paramMap.pipe(map((el) => el.get('id') || ''));
+  public user = toSignal<IUser>(
+    this.params.pipe(switchMap((id) => this.usersService.getUserById(id)))
+  );
+  public userName = computed(() => this.user()?.name);
+  constructor() {
+    effect(() => {
+      if (this.user()) {
+        console.log('Usuario cargado:', this.user());
+      }
+    });
+  }
+  ngOnInit(): void {}
 }
